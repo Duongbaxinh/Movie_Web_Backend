db3b2962614b82cf518a4bc1229dc737368603c2
Duongbaxinh <duongbaxinh2021@gmail.com>
2024-11-13 22:16:49 +0700

    update code

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: "root",
+    password: "123456",
+    database: "web_movie",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER | "avnadmin",
+    password: process.env.DB_PASS,
+    database: "movies",
+    host:
+      process.env.DB_HOST |
+      "mysql-182751c1-duongbaxinh2021-7e43.d.aivencloud.com",
+    port: process.env.DB_PORT | 25482,
+    dialect: "mysql",
+  },
+};
